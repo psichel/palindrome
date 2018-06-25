543f014d6fd3f3b46bbb5583353d31f2c991d982
Peter Sichel <psichel@rbmtechnologies.com>
2018-06-25 16:00:52 -0400

    Initialize repository

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// Reverses a string.
+function reverse(string) {
+  return Array.from(string).reverse().join("");
+}
+
+/*
+// Returns true for a palindrome, false otherwise.
+function palindrome(string) {
+  let processedContent = string.toLowerCase();
+  return processedContent === reverse(processedContent);
+}
+*/
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    let processedContent = this.content.toLowerCase();
+    return processedContent === reverse(processedContent);
+  }
+  
+  // Makes the phrase LOUDER.
+  this.louder = function louder() {
+    let processedContent =  this.content.toUpperCase();
+    return processedContent;
+  };
+
+}
