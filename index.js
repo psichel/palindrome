37000feedef0a0239a5ab46d0821e036b404dbc0
Peter Sichel <psichel@rbmtechnologies.com>
2018-06-26 15:48:42 -0400

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
-}
+module.exports = Phrase;
 
-/*
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
-*/
 
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+  }
+
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+    return this.processedContent() === this.processedContent().reverse();
   }
-  
-  // Makes the phrase LOUDER.
-  this.louder = function louder() {
-    let processedContent =  this.content.toUpperCase();
-    return processedContent;
-  };
-
 }
